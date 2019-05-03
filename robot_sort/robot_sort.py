086906c2c647ff74afbd01d0315dacacdfb9da27
yannafaith <yannafaith97@gmail.com>
2019-05-03 12:44:27 -0700

    cleans up solution

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -127,36 +127,29 @@ class SortingRobot:
             
 
     def sort(self):
-        while(self):
-            self.set_light_on()
-            while(self.can_move_right() == True):
-                self.move_right()
-                if(self.compare_item() == 1):
-                    self.swap_item()
-                    self.set_light_off()
-                self.swap_and_return()
-            if self.light_is_on() == True:
+        self.set_light_on()
+        while(self.can_move_right() == True):
+            self.move_right()
+            if(self.compare_item() == 1):
                 self.swap_item()
-                break
-            else:
-                self.swap_item()
-    
-            while self.can_move_left() == True:
+                self.set_light_off()
+            self.swap_and_return()
+        self.swap_item()
+        if self.light_is_on() == False:
+            while(self.can_move_left() == True):
                 self.move_left()
+            self.sort()
         
         while self.can_move_left():
             self.move_left()
         self.move_right()
         if self.compare_item() == 1:
-            print('big')
             self.swap_item()
             self.move_left()
             self.swap_item()
         elif self.compare_item() == -1:
-            print('small')
             self.move_left()
             self.swap_item()
-        print(f' DONE: {self._item}, {self._position}, {self._list} \n')
 
 
 
